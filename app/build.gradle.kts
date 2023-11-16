af64a90762e413544fe5eb992b7d6b5a12272646
Omer Yildirim <omer.yildirim@commencis.com>
2023-11-16 15:19:20 +0300

    deploy production

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.yildirimomer01.folkz"
         minSdk = 24
         targetSdk = 33
-        versionCode = 2
-        versionName = "1.2"
+        versionCode = 3
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
